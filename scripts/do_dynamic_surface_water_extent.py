fe0f9f047b245ae4ff3af9aadeeb88ad7eba0971
Ronald Dilley <rdilley@usgs.gov>
2014-12-01 22:00:15 +0000

    Re-write, but still needs testing
    git-svn-id: https://surface-water-extent.googlecode.com/svn/branches/DSWE_VERSION_1.0.0_DEV@85 5bc5f250-f465-1fc8-ad61-76a02065d8fc

--- scripts/do_dynamic_surface_water_extent.py
+++ scripts/do_dynamic_surface_water_extent.py
@@ -1,309 +1,159 @@
 #! /usr/bin/env python
 
 '''
-Created on January 29, 2013 by Song Guo, USGS/EROS
+    FILE: do_dynamic_surface_water_extent.py
 
-License:
-  "NASA Open Source Agreement 1.3"
+    PURPOSE: Master script for running scene-based surface water algorithm.
 
-Description:
-  Run the scene-based surface water algorithm, including checking processing
-  status.
+    USAGE: See 'do_dynamic_surface_water_extent.py --help'
 
-Usage:
-  do_surface_water_extent.py --help prints the help message
+    LICENSE: NASA Open Source Agreement 1.3
+
+    HISTORY:
+
+    Date              Programmer               Reason
+    ----------------  ------------------------ -------------------------------
+    Jan/2013          Song Guo                 Initial Implementation
+    Dec/2014          Ron Dilley               Complete Re-write
 '''
 
-import sys
 import os
-import re
-import subprocess
-import datetime
-from optparse import OptionParser
+import commands
+import logging
+from argparse import ArgumentParser
 
-from espa_constants import *
-from espa_logging import log
+from espa_constants import EXIT_SUCCESS
+from espa_constants import EXIT_FAILURE
 
-class Swe():
+
+def execute_cmd(cmd):
     '''
-    Defines the class object for performing surface water extent processing
+    Description:
+      Execute a command line and return the terminal output or raise an
+      exception
+
+    Returns:
+        output - The stdout and/or stderr from the executed command.
     '''
 
-    def __init__(self):
-        pass
+    output = ''
 
-    def runSwe (self, metafile=None, reflectance=None, dem=None,
-                mgt=None, mlt1=None, mlt2=None, b4lt1=None, b4lt2=None,
-                b5lt1=None, b5lt2=None, per_shade=None, write_binary=None,
-                use_ledaps_mask=None, use_zeven_thorne=None, verbose=None,
-                logfile=None, usebin=None):
-        """
-        Description:
-          runSwe will use the parameters passed for the input/output files,
-          logfile, and usebin.  If input/output files are None (i.e. not
-          specified) then the command-line parameters will be parsed for this
-          information.  The scene-based DEM and SWE applications are then
-          executed on the specified input files.  If a log file was specified,
-          then the output from each application will be logged to that file.
+    (status, output) = commands.getstatusoutput(cmd)
 
-        Inputs:
-          metafile - name of the Landsat metadata file to be processed
-          reflectance - name of the input TOA reflectance HDF file to be
-              processed
-          dem - name of an ENVI formatted DEM file
-          mgt, mlt1, mlt2, b4lt1, b4lt2, b5lt1, b5lt2, per_shade -
-              these are threashold values used in the SWE algorithm
-          logfile - name of the logfile for logging information; if None then
-              the output will be written to stdout
-          usebin - this specifies if the DEM and SCE exes reside in the $BIN
-              directory; if None then the DEM and SCA exes are expected to be
-              in the PATH
+    if status < 0:
+        message = "Application terminated by signal [%s]" % cmd
+        if len(output) > 0:
+            message = ' Stdout/Stderr is: '.join([message, output])
+        raise Exception(message)
 
-        Returns:
-            ERROR - error running the DEM or SWE application
-            SUCCESS - successful processing
+    if status != 0:
+        message = "Application failed to execute [%s]" % cmd
+        if len(output) > 0:
+            message = ' Stdout/Stderr is: '.join([message, output])
+        raise Exception(message)
 
-        Notes:
-          1. The script obtains the path of the metadata file and changes
-             directory to that path for running the scene-based DEM and SCA
-             code.  If the metafile directory is not writable, then this
-             script exits with an error.
-          2. If the metadata file is not specified and the information is
-             going to be grabbed from the command line, then it's assumed all
-             the parameters will be pulled from the command line.
-          3. Currently the raw binary products are automatically written in
-             addition to the HDF-EOS product for the SWE algorithm.  The raw
-             binary products will be used for downstream processing, but only
-             the output HDF-EOS product should be delivered to the general
-             public.
-        """
+    if os.WEXITSTATUS(status) != 0:
+        message = "Application [%s] returned error code [%d]" \
+                  % (cmd, os.WEXITSTATUS(status))
+        if len(output) > 0:
+            message = ' Stdout/Stderr is: '.join([message, output])
+        raise Exception(message)
 
-        # define a parser to read arguments from the command line
-        parser = OptionParser()
-
-        # specify the command line agruments to read
-        parser.add_option ('-f', '--metafile', type='string', dest='metafile',
-            help="name of Landsat MTL file", metavar='FILE')
-
-        parser.add_option ('-r', '--reflectance', type='string',
-            dest='reflectance',
-            help="name of SR or TOA reflectance HDF file", metavar='FILE')
-
-        parser.add_option ('--dem', type='string',
-            dest='dem',
-            help="name of an ENVI formatted DEM file", metavar='FILE')
-
-        parser.add_option('--mgt', dest='mgt',
-            action='store', default=0.123, help="mgt value")
-
-        parser.add_option('--mlt1', dest='mlt1',
-            action='store', default=-0.5, help="mlt1 value")
-
-        parser.add_option('--mlt2', dest='mlt2',
-            action='store', default=-0.4, help="mlt2 value")
-
-        parser.add_option('--b4lt1', dest='b4lt1',
-            action='store', default=1500, help="b4lt1 value")
-
-        parser.add_option('--b4lt2', dest='b4lt2',
-            action='store', default=1500, help="b4lt2 value")
-
-        parser.add_option('--b5lt1', dest='b5lt1',
-            action='store', default=1000, help="b5lt1 value")
-
-        parser.add_option('--b5lt2', dest='b5lt2',
-            action='store', default=1700, help="b5lt2 value")
-
-        parser.add_option('--per_shade', dest='per_shade',
-            action='store', default=3.0, help="percent shade value")
-
-        parser.add_option ('--usebin', dest='usebin', default=False,
-            action='store_true',
-            help="use BIN environment variable to find DEM and SCA apps")
-
-        parser.add_option ('-l', '--logfile', type='string', dest='logfile',
-            help="name of optional log file", metavar='FILE')
-
-        parser.add_option('-b', '--write_binary',
-            action='store_true', dest='write_binary', default=True,
-            help="don't output results to binary file")
-
-        parser.add_option('-u', '--use_ledaps_mask',
-            action='store_false', dest='use_ledaps_mask', default=False,
-            help="use ledaps cloud/shadow results")
-
-        parser.add_option('-z', '--use_zeven_thorne', action='store_false',
-            dest='use_zeven_thorne', default=False,
-            help="use Zevenburgen Thorne slope algorithm")
-
-        parser.add_option('-q', '--quiet', action='store_true', dest='verbose',
-            default=True, help="don't print status messages to stdout")
-
-        # read any command line arguments
-        (options, args) = parser.parse_args()
-    
-        # if not provided through the method arguments grab the value from the
-        # command line options which will be populated with the default value
-        # if specified
-
-        if usebin == None:
-            usebin = options.usebin
-
-        if logfile == None:
-            logfile = options.logfile
-
-        if write_binary == None:
-            write_binary = options.write_binary
-
-        if use_ledaps_mask == None:
-            use_ledaps_mask = options.use_ledaps_mask
-
-        if use_zeven_thorne == None:
-            use_zeven_thorne = options.use_zeven_thorne
-
-        if verbose == None:
-            verbose = options.verbose
-
-        if mgt == None:
-            mgt = options.mgt
-
-        if mlt1 == None:
-            mlt1 = options.mlt1 
-
-        if mlt2 == None:
-            mlt2 = options.mlt2 
-
-        if b4lt1 == None:
-            b4lt1 = options.b4lt1 
-
-        if b4lt2 == None:
-            b4lt2 = options.b4lt2 
-
-        if b5lt1 == None:
-            b5lt1 = options.b5lt1 
-
-        if b5lt2 == None:
-            b5lt2 = options.b5lt2 
-
-        if per_shade == None:
-            per_shade = options.per_shade 
-
-        ##### Verify that required parameters are present #####
-
-        # metadata file
-        if metafile == None:
-            metafile = options.metafile
-            if metafile == None:
-                parser.error ("missing metafile command-line argument");
-                return ERROR
-
-        # SR or TOA reflectance file
-        if reflectance == None:
-            reflectance = options.reflectance
-            if reflectance == None:
-                parser.error ("missing input reflectance file command-line" \
-                    " argument");
-                return ERROR
-                       
-        # ENVI DEM file
-        if dem == None:
-            dem = options.dem
-            if dem == None:
-                parser.error ("missing input dem file command-line" \
-                    " argument");
-                return ERROR
-                       
-        # let the world know what we are processing
-        msg = "SWE processing of LEDAPS reflectance file: %s" % reflectance
-        log (msg)
-        
-        # should we expect the DEM and SWE applications to be in the PATH or
-        # in the BIN directory?
-        if usebin:
-            # get the BIN dir environment variable
-            bin_dir = os.environ.get('BIN') + '/'
-            msg = "BIN environment variable: %s" % bin_dir
-            log (msg)
-        else:
-            # don't use a path to the DEM/SWE applications
-            bin_dir = ''
-            msg = "DEM and SWE executables expected to be in the PATH"
-            log (msg)
-        
-        # make sure the metadata file exists
-        if not os.path.isfile(metafile):
-            msg = "Error: metadata file does not exist or is not" \
-                " accessible: " + metafile
-            log (msg)
-            return ERROR
-
-        # use the base metadata filename and not the full path.
-        base_metafile = os.path.basename (metafile)
-        msg = "Processing metadata file: %s" % base_metafile
-        log (msg)
-        
-        # get the path of the MTL file and change directory to that location
-        # for running this script.  save the current working directory for
-        # return to upon error or when processing is complete.  Note: use
-        # abspath to handle the case when the filepath is just the filename
-        # and doesn't really include a file path (i.e. the current working
-        # directory).
-        mydir = os.getcwd()
-        metadir = os.path.dirname (os.path.abspath (metafile))
-        if not os.access(metadir, os.W_OK):
-            msg = "Path of metadata file is not writable: %s.  Script needs" \
-                " write access to the metadata directory." % metadir
-            log (msg)
-            return ERROR
-
-        msg = "Changing directories for SWE processing: %s" % metadir
-        log (msg)
-        os.chdir (metadir)
-
-        # run SWE algorithm, checking the return status of each module.
-        # exit if any errors occur.
-        cmd = ['%sscene_based_swe' % bin_dir,
-               '--reflectance=%s' % reflectance,
-               '--dem=%s' % dem,
-               '--mgt=%f' % mgt,
-               '--mlt1=%f' % mlt1,
-               '--mlt2=%f' % mlt2,
-               '--b4lt1=%d' % b4lt1,
-               '--b4lt2=%d' % b4lt2,
-               '--b5lt1=%d' % b5lt1,
-               '--b5lt2=%d' % b5lt2,
-               "--per_shade=%f" % per_shade,
-               "--verbose"]
-
-        if write_binary:
-            cmd = cmd + ['--write_binary']
-
-        print "Executing: scene_based_swe command: %s" % ' '.join(cmd)
-        try:
-            (output) = subprocess.check_output (cmd)
-        except subprocess.CalledProcessError, e:
-            msg = "Error running scene_based_swe.  Processing will terminate."
-            log (msg)
-            os.chdir (mydir)
-            return ERROR
-
-        log (output)
-            
-        # successful completion.  return to the original directory.
-        os.chdir (mydir)
-        msg = "Completion of scene based surface water extent."
-        log (msg)
-
-        return SUCCESS
-    #### End of runSwe ####
-#### End of Swe class ####
+    return output
 
 
 if __name__ == '__main__':
-    return_value = Swe().runSwe()
+    '''
+    Description:
+        Execute the main code.
+    '''
 
-    if (return_value != SUCCESS):
-        sys.exit (EXIT_FAILURE)
-    else:
-        sys.exit (EXIT_SUCCESS)
+    # Create a command line argument parser
+    description = ("Build the command line and then kick-off the Dynamic"
+                   " Surface Water Extent application")
+    parser = ArgumentParser(description=description)
 
+    # ---- Add parameters ----
+    # Required parameters
+    parser.add_argument('--xml',
+                        action='store', dest='xml_filename', required=True,
+                        help="The XML metadata file to use")
+
+    # Optional parameters
+    default_wigt = '0.015'
+    parser.add_argument('--wigt',
+                        action='store', dest='wigt', default=default_wigt,
+                        help=("Modified Normalized Difference Wetness Index"
+                              " Threshold between 0.00 and 2.00"
+                              " (default value is {0})".format(default_wigt)))
+
+    default_awgt = '0.0'
+    parser.add_argument('--awgt',
+                        action='store', dest='awgt', default=default_awgt,
+                        help=("Automated Water Extent Shadow"
+                              " Threshold between -2.00 and 2.00"
+                              " (default value is {0})".format(default_awgt)))
+
+    default_pswt = '0.0'
+    parser.add_argument('--pswt',
+                        action='store', dest='pswt', default=default_pswt,
+                        help=("Partial Surface Water"
+                              " Threshold between -2.00 and 2.00"
+                              " (default value is {0})".format(default_pswt)))
+
+    default_pswnt = '1500'
+    parser.add_argument('--pswnt',
+                        action='store', dest='pswnt', default=default_pswnt,
+                        help=("Partial Surface Water NIR"
+                              " Threshold between 0 and data maximum value"
+                              " (default value is {0})".format(default_pswnt)))
+
+    default_pswst = '1000'
+    parser.add_argument('--pswst',
+                        action='store', dest='pswst', default=default_pswst,
+                        help=("Partial Surface Water SWIR1"
+                              " Threshold between 0 and data maximum value"
+                              " (default value is {0})".format(default_pswst)))
+
+    parser.add_argument('--verbose',
+                        action='store_true', dest='verbose', default=False,
+                        help=("Should intermediate messages be printed?"
+                              " (default value is False)"))
+
+    # Parse the command line parameters
+    args = parser.parse_args()
+
+    # setup the default logger format and level. log to STDOUT.
+    logging.basicConfig(format=('%(asctime)s.%(msecs)03d %(process)d'
+                                ' %(levelname)-8s'
+                                ' %(filename)s:%(lineno)d:'
+                                '%(funcName)s -- %(message)s'),
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+
+    # get the logger
+    logger = logging.getLogger(__name__)
+
+    # Build the command line
+    cmd = ['dswe', '--xml', args.xml_filename,
+           '--wigt', args.wigt,
+           '--awgt', args.awgt,
+           '--pswt', args.pswt,
+           '--pswnt', args.pswnt,
+           '--pswst', args.pswst]
+
+    if args.verbose:
+        cmd.append('--verbose')
+
+    # Convert to a string
+    cmd = ' '.join(cmd)
+
+    logger.info("Executing [{0}]".format(cmd))
+
+    try:
+        execute_cmd(cmd)
+    except Exception, e:
+        logger.fatal(str(e))
+        return EXIT_FAILURE
+
+    return EXIT_SUCCESS
