df70c81b93c99b74f122a3246afbe7ee6153ef6e
Ronald Dilley <rdilley@usgs.gov>
2015-01-26 15:24:07 +0000

    Updated
    git-svn-id: https://surface-water-extent.googlecode.com/svn/branches/DSWE_VERSION_1.0.0_DEV@109 5bc5f250-f465-1fc8-ad61-76a02065d8fc

--- src/const.h
+++ src/const.h
@@ -2,23 +2,22 @@
 #ifndef CONST_H
 #define CONST_H
 
-#ifndef SUCCESS
-#define SUCCESS  0
-#endif
-
-#ifndef ERROR
-#define ERROR   -1
-#endif
 
 #define MODULE_NAME "DSWE"
 #define DSWE_VERSION "1.0.0"
+
+
+#define SUCCESS 0
+#define ERROR 1
+
 
 #define PRODUCT_NAME "dswe"
 #define BAND_NAME "dswe"
 #define SHORT_NAME "DSWE"
 #define LONG_NAME "dynamic surface water extent"
 
-/* These are use in arrays, so they are position dependent */
+
+/* These are used in arrays, and they are position dependent */
 typedef enum
 {
     I_BAND_BLUE,
@@ -27,14 +26,19 @@ typedef enum
     I_BAND_NIR,
     I_BAND_SWIR1,
     I_BAND_SWIR2,
-    I_BAND_CFMASK,
+    I_BAND_CFMASK, /* This band and above are all from the XML */
+    I_BAND_DEM,
     MAX_INPUT_BANDS
 } Input_Bands_e;
 
+
 #define MINSIGMA 1e-5
+
 
 #define DSWE_NO_DATA_VALUE -9999
 
+
 #define MAX_DATE_LEN 28
+
 
 #endif /* CONST_H */
